94abd50d46566a9e126c8fc9b7b04b9affa5646c
autumn-ragland <raglanda998@gmail.com>
2019-08-27 14:16:35 -0500

    Graded 08-27-2019 14:14:52

--- classwork.py
+++ classwork.py
@@ -7,7 +7,6 @@
 # b) Print the size of the array
 # c) Delete the second element.
 # d) Print the 3rd element.
-
 def prob1():
     arrayForProblem2 = ["Kenn", "Kevin", "Erin", "Meka"]
     print(arrayForProblem2[2])
@@ -18,52 +17,32 @@ def prob1():
 # ### Problem 2:
 # ##### We will keep having this problem until EVERYONE gets it right without help
 # Create a function that has a loop that quits with ‘q’. If the user doesn't enter 'q', ask them to input another string.
-
 def prob2():
     userInput =''
     while userInput != 'q':
         userInput = input('Enter some data, press "q" to quit: ')
 
-### Problem 3:
-# Create a function that contains a collection of information for the following. After you create the collection do the instructions below that.
-# ```
-# Jonathan/John
-# Michael/Mike
-# William/Bill
-# Robert/Rob
-# ```
-# a) Print the collection
-# b) Print William's nickname
-
 ### Problem 4:
 # Create an array of 5 numbers. Using a loop, print the elements in the array reverse order. Do not use a function
-
-
-# numberArray = [10,20,30,40,50]
-# for a in reversed(numberArray):
-#     print(a)
-
+numberArray = [10,20,30,40,50]
+for a in reversed(numberArray): # !! : DO NOT USE A FUNCTION
+    print(a)
 
 ### Problem 5:
 # Create a function that will have a hard coded array then ask the user for a number. Use the userInput to state how many numbers in an array are higher, lower, or equal to it.
-
 def prob5(givenArray):
    for x in givenArray:
-       while userInput != givenArray:
+       while userInput != givenArray: # !! : this condition will never be met 
            if userArray > givenArray:
-               print(len(givenArray))
+               print(len(givenArray)) # !! : this does not meet the requirement 
        else:
         print("something")
-
-
-
-
 userArray = [12,27,-2,59]
-userInput =userArray.append(int(input('Enter a number')))
+userInput = userArray.append(int(input('Enter a number')))
 prob5(userArray)
 
-# prob1()
-# prob2()
-# prob3()
-# prob4()
+prob1()
+prob2()
+prob3()
+prob4()
 
